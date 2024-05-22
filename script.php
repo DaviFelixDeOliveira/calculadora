2dc3e662d7040ff781ade8761885dc970fae1ca7
davi.oliveira153@etec.sp.gov.br <davi.oliveira153@etec.sp.gov.br>
2024-05-22 17:54:44 -0300

    Atividade Usando laçoes de repetição com For

---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+
+$contador;
+$numero = $_POST['numero'];
+$resultado;
+
+for ($contador=0; $contador <=10 ; $contador++) { 
+    
+
+    $resultado = $numero * $contador;
+echo " $numero" . " X " . "$contador " . "= " . "$resultado ";
+echo "<br>";
+
+}
+
+
+
+?>
