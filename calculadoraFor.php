2688050870cde14781b05f8a42e4a35f2f4856e5
Bucharelli <robertobucharelli@etec.sp.gov.br>
2024-05-23 21:35:57 -0300

    salvando

--- calculadoraFor.php
+++ calculadoraFor.php
@@ -1,12 +1,12 @@
 <?php
-$contador;
-$numero = $_POST['numero'];
-$resultado;
+    $contador;
+    $numero = $_POST['numero'];
+    $resultado;
 
-for ($contador=0; $contador <=10 ; $contador++)
-{ 
-    $resultado = $numero * $contador;
-    echo " $numero" . " X " . "$contador " . "= " . "$resultado ";
-    echo "<br>";
-}
+    for ($contador=0; $contador <=10 ; $contador++)
+    { 
+        $resultado = $numero * $contador;
+        echo "$numero" . " X " . "$contador " . "= " . "$resultado";
+        echo "<br>";
+    }
 ?>
